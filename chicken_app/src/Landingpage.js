cf77c4b4e9d17ed92c52d96a83a2fbf3f8e44847
Yellow987 <daryldsouza123@gmail.com>
2022-09-27 12:43:12 -0700

    landing page formatting cleanup

--- chicken_app/src/Landingpage.js
+++ chicken_app/src/Landingpage.js
@@ -4,6 +4,8 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import chickenImage from './Images/chickenImage.jpg';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
 export const Landingpage = ({sendDataToPage})=> {
     const styles = {
         twoButtons: {
@@ -22,29 +24,30 @@ export const Landingpage = ({sendDataToPage})=> {
             borderRadius : '8px'
         },
         toNavMenuMargin : {
-            marginTop:'143px'
+            marginTop: { sm:'143px', xs:'40px' }
         }
         
     };
     return(
-        <Container maxWidth="lg" style={styles.toNavMenuMargin}>
+        <Container maxWidth="lg" sx={styles.toNavMenuMargin}>
             <Grid container spacing={2} alignItems='center'>
                 <Grid item xs={12} sm={6}>
                     <Typography variant="h4" component="h1" sx={{ flexGrow: 1 }} fontWeight='bold' style={{marginBottom:'16px'}}>
                     Asia’s trusted directory of cage-free egg sellers
                     </Typography>
                     <Typography variant="h6" component='p' style={{marginBottom:'48px'}} >Browse the profiles of cage-free sellers, or create a profile yourself and get discovered!</Typography>
-                    <div style={styles.twoButtons}>
+                    <Box style={styles.twoButtons}>
                         <Button variant="contained" style={styles.marginButton} onClick={() => {
                             sendDataToPage('formpart1');
                             }}>Sign up as a seller</Button>
                         <Paper elevation={3}><Button variant="outlined" style={styles.height}>Sign up as a buyer</Button></Paper>
-                    </div>
+                    </Box>
                 </Grid>
-                <Grid item xs={12} sm={6} sx={{display: 'flex', alignItems: 'center'}} justifyContent="flex-end" >
-                    <img src={chickenImage} alt="chickenImage" style={styles.imageDimensions} />
+                <Grid item xs={12} sm={6} sx={{ alignItems: 'center'}} justifyContent="flex-end" >
+                    <Box component="img" src={chickenImage} alt="chickenImage" sx={{ width:1 }}/>
                 </Grid>
             </Grid>
+            <Typography variant="h6" component='p' >Always free</Typography>
       </Container>
     )
 }
