7b51b21627a2baa5b3904f0f7670039889227857
Yellow987 <daryldsouza123@gmail.com>
2022-09-26 19:24:57 -0700

    before liveshare

--- chicken_app/src/App.js
+++ chicken_app/src/App.js
@@ -1,37 +1,50 @@
-import './App.css';
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { createTheme } from '@mui/system';
-import { ThemeProvider } from '@emotion/react';
-import { green, purple } from '@mui/material/colors';
+import Grid from '@mui/material/Grid';
+import chickenImage from './Images/chickenImage.jpg';
+import Box from '@mui/material/Box';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3FAB94',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#FF0000',
+    },
+  },
+});
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: purple[500]
-      }
-    }
-  })
-  
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-        </div>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Cage Free Hub
+          </Typography>
+          <Button>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Asia’s trusted directory of cage-free egg sellers
+          </Typography>
+          <Button variant="contained">Create Acct</Button>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={{display: 'flex', alignItems: 'center'}} >
+          <img src={chickenImage} alt="chickenImage"/>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
---
+++ chicken_app/src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/system';
+import { purple } from '@mui/material/colors';
+
+
+const theme = createTheme({
+    palette: {
+        main: purple[500]
+    }
+  })
+
+  export default theme
