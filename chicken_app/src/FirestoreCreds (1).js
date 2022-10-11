ebe9074260dd409c3c4c2bf32235ebbebbe8ad21
sophia pung <sophiagracepung@gmail.com>
2022-10-11 00:31:36 -0700

    adding FirestoreCreds file to the src files

---
+++ chicken_app/src/FirestoreCreds (1).js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAUTMx89j6lV6-R5EGn0yRWtAL5tu2SjLs",
+    authDomain: "freerangeeggfarm-26736.firebaseapp.com",
+    projectId: "freerangeeggfarm-26736",
+    storageBucket: "freerangeeggfarm-26736.appspot.com",
+    messagingSenderId: "471292011029",
+    appId: "1:471292011029:web:b24d4e5a4f993aa860641c"
+  };
