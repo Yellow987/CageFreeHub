5f53de48b8af790ee6c450eaa63b7de67fa50fcb
Yelllow Daryl <daryldsouza123@gmail.com>
2022-10-14 20:21:37 -0700

    Revert "adding confirm email mui component (#8)" (#9)
    This reverts commit 1d59bd5bbfc373542da4ad9e60712ec51d07a626.

--- chicken_app/src/Pages/Profile/ConfirmEmail.js
+++ chicken_app/src/Pages/Profile/ConfirmEmail.js
@@ -1,57 +1,9 @@
-import { React, useState, useRef } from "react";
-import MailIcon from "@mui/icons-material/Mail";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
-import { getAuth, sendEmailVerification } from "firebase/auth";
-
-import { Box, ThemeProvider, createTheme, Typography } from "@mui/material";
-import { borders } from "@mui/system";
-import { Button } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    background: {
-      paper: "#f1faf8",
-    },
-    typography: {
-      body1: {
-        color: "#6bb09e",
-      },
-    },
-    text: {
-      primary: "#6bb09e",
-    },
-  },
-});
+import React from 'react'
 
 function ConfirmEmail() {
-  const [emailSent, setEmailSent] = useState({ isEmailSent:true, emailSentDetails:"Email verification sent!"});
-  const auth = getAuth();
-  console.log("emailSent", emailSent);
-  sendEmailVerification(auth.currentUser).then(() => {
-    setEmailSent({ isEmailSent:true, emailSentDetails:"Email verification sent!"});
-  });
   return (
-    <ThemeProvider theme={theme}>
-      <Box align='flex' mx={{ sm:'auto', xs:'24px' }} sx={{maxWidth:'700px', mt:{ sm:'116px', xs:'24px'}, borderColor: 'text.primary', color: "success.main" }}>
-        <Alert icon={<MailIcon fontSize="inherit" sx={{marginTop: "3px", color: '#6bb09e'}}/>} severity="success" sx={{backgroundColor: "background.paper", marginTop: "15px", marginBottom: "-5px", }}>
-          <AlertTitle variant="h5" sx={{color: '#6bb09e', fontColor: "text.primary", fontWeight: "bold" }}>
-            Confirm your email
-          </AlertTitle>
-          <Typography style={{color: '#6bb09e'}}>
-            An email with a confirmation link has been sent to the provided
-            email address. Please confirm this link for the opportunity to view
-            our directory of cage-free egg sellers.{" "}
-          </Typography>
-        </Alert>
-        <Button variant="h5" sx={{fontWeight: "bold", fontColor: "text.primary", backgroundColor: "background.paper", marginTop: "15px", marginBottom: "-5px", }} type="submit" fullWidth>
-          Resend confirmation email
-        </Button>
-        <Alert severity='error' display={emailSent.isEmailSent ? "block" : "none"}>
-          <AlertTitle>{emailSent.emailSentDetails}</AlertTitle>
-        </Alert>
-      </Box>
-    </ThemeProvider>
-  );
+    <div>ConfirmEmail</div>
+  )
 }
-export default ConfirmEmail;
+
+export default ConfirmEmail
