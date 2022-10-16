e7c68f16f08a9bee03d43bfdd08d29975c913d60
Yellow987 <daryldsouza123@gmail.com>
2022-10-15 18:24:25 -0700

    updates

--- chicken_app/src/Pages/Profile/ProductDetails.js
+++ chicken_app/src/Pages/Profile/ProductDetails.js
@@ -1,6 +1,6 @@
 import { Alert, Typography } from '@mui/material'
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import { useOutletContext } from 'react-router'
 import { Box } from '@mui/system'
@@ -8,6 +8,7 @@ import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 
 function ProductDetails() {
   const [setPage, goToPage, setGoToPage] = useOutletContext()
+  const [productDetails, setProductDetails] = useState({})
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -39,6 +40,8 @@ function ProductDetails() {
         <Typography variant='p_default' color='#3FAB94' >All information provided is completely confidential. We do not share information with third parties, and buyers must be confirmed by us to access profiles</Typography>
       </Alert>
       <Typography variant='p_default_bold' sx={{ marginTop:4 }}>Cage-free egg types</Typography>
+
+
     </Box>
   )
 }
