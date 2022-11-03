15d1773125940162d9fb6c0e28d30a176a1fa42b
Yellow987 <daryldsouza123@gmail.com>
2022-11-02 22:44:39 -0700

    changes

--- src/Components/ProfileProgressBar.js
+++ src/Components/ProfileProgressBar.js
@@ -41,7 +41,9 @@ function ProfileProgressBar() {
           locations: [{city:'',country:''}],
 
           //Contact
-
+          name: '',
+          jobTitle: '',
+          contactChannels: {phone: '', whatsapp: '', wechat: ''},
 
           //Product details
           productDetails: {},
