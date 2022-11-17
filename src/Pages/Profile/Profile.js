6e40091927dc9e1d68c3e0dd0fcd9e4009056947
deeptiboddapati <deeptiboddapati@gmail.com>
2022-11-17 01:53:14 -0600

    Added checking for Admin user

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -4,32 +4,43 @@ import { Box, Typography,  Divider, Link  } from '@mui/material';
 import AdminApprovalOptions from '../../Components/AdminApprovalOptions';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import Carousel from 'react-material-ui-carousel';
+import { useAuth } from './../../AuthContext';
 function Profile() {
   const { id } = useParams()
   const [data, setData] = useState(null)
+  const [ userData, setUserData ] = useState();
   const db = getFirestore()
   const docRef = useCallback(() => { return doc(db, "farms", id) }, [db, id])
+  const { currentUser } = useAuth()
+  const userDocRef = useCallback(()=>{ return doc(db,"users", currentUser.uid )}, [db, currentUser.uid])
 
   useEffect(() => {
     getDoc(docRef()).then((doc) => {
       setData(doc.data())
     })
+    getDoc(userDocRef()).then((doc) => {
+      setUserData(doc.data())
+    })
   }, [docRef])
-  console.log(data);
-  // console.log( data.locations)
+  console.log()
+
   return (
     <>
       {data && <Box sx={{ mx:'24px', marginTop:'56px' }}>
-        <AdminApprovalOptions props={{ data:data, docRef:docRef() }}/>
-        {id}
-        <Box sx={{ background:'#F5F7F8', padding:2 }}>
-          <Typography variant='p_large' fontWeight='bold'>Awaiting approval</Typography>
-          <Typography variant='p_large'>
-            <br/>Our team is reviewing your profile for approval <br/><br/>
-            We’ll send a text to the provided number once the profile is approved<br/><br/>
-            This will take 72 hours at most
-          </Typography>
-        </Box>
+        {
+          userData.isAdmin &&
+          <>
+            <AdminApprovalOptions props={{ data:data, docRef:docRef() }}/>
+            <Box sx={{ background:'#F5F7F8', padding:2 }}>
+              <Typography variant='p_large' fontWeight='bold'>Awaiting approval</Typography>
+              <Typography variant='p_large'>
+                <br/>Our team is reviewing your profile for approval <br/><br/>
+                We’ll send a text to the provided number once the profile is approved<br/><br/>
+                This will take 72 hours at most
+              </Typography>
+            </Box>
+          </>
+        }
         <Typography variant='h1' sx={{ marginTop:7 }}></Typography>
         <Box sx={{display:'flex', margin:'0 auto', justifyContent:'space-between', maxWidth:'1060px'}}>
             <Box width='620px'>
