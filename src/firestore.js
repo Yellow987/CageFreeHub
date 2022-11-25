0902426eefd5c7c8643ee132b8b582cf889ea144
deeptiboddapati <deeptiboddapati@gmail.com>
2022-11-24 21:51:49 -0600

    Cleaned up un-needed change

--- src/firestore.js
+++ src/firestore.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-    getFirestore, collection, getDocs, onSnapshot, doc
+    getFirestore, collection, getDocs
 } from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
